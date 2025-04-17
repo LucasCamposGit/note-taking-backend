f5ded815d5711327d8404aa51a40492589857e75
LucasCamposGit <lucascpn2501@gmail.com>
2025-04-16 21:46:27 -0300

    s

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "https://mini-notes-jade.vercel.app")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			if r.Method == "OPTIONS" {
